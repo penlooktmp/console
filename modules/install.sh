7b06eb35502b70735428eef55b5d2cefa844d1f8
loint <loint@penlook.com>
2015-01-29 19:14:25 +0700

    Add modules for cli

---
+++ modules/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright 2014 Penlook Development Team. All rights reserved.
+# Use of this source code is governed by
+# license that can be found in the LICENSE file.
+# Author : Loi Nguyen <loint@penlook.com>
+
+main="penlook"
+touch $main
+echo "" > $main
+
+include() {
+	cat $1 >> ./modules/$main.sh
+}
+
+include "util"
+include "build"
+include "test"
+include "start"
+include "stop"
+
+sudo chmod +x $main
+sudo cp -rf $main /usr/bin/$main
+cat /usr/bin/penlook
