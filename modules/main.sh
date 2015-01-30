3f9c7037859db4311c4e75b45aa2bbb053147c08
loint <loint@penlook.com>
2015-01-31 03:33:34 +0700

    Fixing bug in compare string and variable

--- modules/main.sh
+++ modules/main.sh
@@ -7,13 +7,13 @@
 # CLI Main
 main() {
 
-	if [ ! -e $1 ]
+	if [ ! -e "$1" ]
 	then
 		if [ ! -e "~/.ssh/id_rsa" ]
 		then
 			$1_main "${@:2}"
 		else
-			if [ $1 != "auth" ] && [ $1 != "help" ] && [ $1 != "version" ]
+			if [ "$1" != "auth" ] && [ "$1" != "help" ] && [ "$1" != "version" ]
 			then
 				echo "Please using below command to login"
 				echo "\$ penlook auth your_email"
