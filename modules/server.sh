da41bb1f594c8f014edf9133f75c19a1c983b9d3
loint <loint@penlook.com>
2015-02-04 07:31:48 +0700

    Add server to penlook cli

--- modules/server.sh
+++ modules/server.sh
@@ -5,9 +5,7 @@
 # Loi Nguyen <loint@penlook.com>
 
 server_main() {
-	echo "Master : 192.168.1.1 > running"
-	echo "Worker : 192.168.1.2 > stop"
-	echo "Worker : 192.168.1.3 > running"
-	echo "Worker : 192.168.1.4 > stop"
-	echo "Worker : 192.168.1.5 > running"
+	SERVER=$(require "console/server")
+	cd $SERVER
+	./main.rb $@
 }
