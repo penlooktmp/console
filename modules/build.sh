164999c020f28a5614a8c91f0868e21efb0a8000
loint <loint@penlook.com>
2015-02-13 18:47:38 +0700

    Fixing wrong new line in Ubuntu, implement build project

--- modules/build.sh
+++ modules/build.sh
@@ -24,5 +24,53 @@
 # +--------------------------------------------------------------------------+
 
 build_main() {
-	echo "BUILD"
+
+	USER=`whoami`
+	ROOT="/home/$USER/src/github.com/penlook"
+	if [ -d $ROOT ]
+	then
+		build_make $ROOT
+	else
+		echo "Can not build without source !"
+	fi
+
 }
+
+build_make() {
+
+	cd $1
+	cd backend
+	zephir build
+
+	build_copy_ini
+	build_restart_service
+
+}
+
+build_restart_service() {
+
+	# For Centos
+	[[ -e "/etc/init.d/php-fpm" ]] && sudo service php-fpm restart
+
+	# For Ubuntu
+	[[ -e "/etc/init.d/php5-fpm" ]] && sudo service php5-fpm restart
+
+}
+
+build_copy_ini() {
+
+	# For Centos
+	[[ -e "/etc/php.d/zzz-phalcon.ini" ]] && echo $'extension=phalcon.so \nextension=app.so' > "/etc/php.d/zzz-phalcon.ini"
+
+	# For Ubuntu
+	[[ -e "/etc/php5/cli/conf.d/90-phalcon.ini" ]] && echo $'extension=phalcon.so \nextension=app.so' > "/etc/php5/cli/conf.d/90-phalcon.ini"
+
+}
+
+
+
+
+
+
+
+
