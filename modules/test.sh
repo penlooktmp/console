fa281bb69143eecc61ce5dbd7313b6236c988adf
naviet <naviet2308@gmail.com>
2015-03-21 16:33:42 +0700

    fix test backend

--- modules/test.sh
+++ modules/test.sh
@@ -1,9 +1,22 @@
 
 test_main() {
-	echo "BUILD"
+	if [ ! -e $1 ]
+	then
+		USER=`whoami`
+		if [ ! -d /home/$USER/src/github.com/penlook/$1 ]
+		then
+			test_$1
+		else
+			echo "The configuration has not yet setup"
+		fi
+	else
+		test_backend
+	fi
+}
+
+test_backend(){
 	USER=`whoami`
 	cd  /home/$USER/src/github.com/penlook/backend/
 	chmod +x test.sh
 	./test.sh
 }
-
