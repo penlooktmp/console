c52b125234c2ca998094be7aa7d2374c5aef90ba
naviet <naviet2308@gmail.com>
2015-03-22 09:50:18 +0700

    add help for command test

--- modules/test.sh
+++ modules/test.sh
@@ -2,21 +2,51 @@
 test_main() {
 	if [ ! -e $1 ]
 	then
-		USER=`whoami`
-		if [ -d /home/$USER/src/github.com/penlook/$1/unit-test.sh ]
+		if [ "$1" != "all" ]
 		then
-			test_$1
+			test $1
 		else
-			echo "The configuration has not yet setup"
+			test_all
 		fi
 	else
-		test_backend
+		test_option
 	fi
 }
 
-test_backend(){
+test(){
 	USER=`whoami`
-	cd  /home/$USER/src/github.com/penlook/backend/
-	chmod +x test.sh
-	./test.sh
+	if [ -d /home/$USER/src/github.com/penlook/$1/unit-test.sh ]
+	then
+		cd  /home/$USER/src/github.com/penlook/$1/
+		chmod +x test.sh
+		./test.sh
+	else
+		echo "The configuration for " $1 " has not yet setup"
+	fi
+}
+
+test_all(){
+	test backed
+	test frontend
+	test service
+	test server
+	test admin
+	test console
+	test setup
+}
+
+test_option() {
+	echo
+	echo "Usage: penlook test <parameter>"
+	echo "Parameters:"
+    echo
+    echo "  all     	Test all"
+    echo "  backend 	Test backend"
+    echo "  frontend 	Test frontend"
+    echo "  service 	Test service"
+    echo "  server 	Test server"
+    echo "  admin 	Test admin"
+    echo "  console 	Test console"
+    echo "  setup 	Test setup"
+    echo
 }
